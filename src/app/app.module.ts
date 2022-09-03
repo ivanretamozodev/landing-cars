1ceb03bcdba3da45d9d07806e4e52ee42aab3173
ivanretamozo <retamozoi445@gmail.com>
2022-09-03 19:32:20 -0300

    carousel añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent, HeroComponent, CarouselComponent],
+  imports: [BrowserModule, AppRoutingModule, IvyCarouselModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
