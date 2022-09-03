c1310be7dbd8685999d4fb89e4093077255ee7d4
ivanretamozo <retamozoi445@gmail.com>
2022-09-03 18:14:58 -0300

    hero basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
